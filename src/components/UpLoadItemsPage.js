f1622faade4bed0c4b4975a7cc97ecba6f07781d
Steven Yi <stevenjxhc@gmail.com>
2024-09-02 01:00:45 -0500

    add category to upload items request

--- src/components/UpLoadItemsPage.js
+++ src/components/UpLoadItemsPage.js
@@ -44,8 +44,7 @@ function UploadItems() {
     const formData = new FormData();
     formData.append("name", values.name);
 
-    //TODO add category
-    // formData.append("category", values.category);
+    formData.append("category", values.category);
 
     formData.append("price", values.price);
     formData.append("description", values.description);
