e431e155a062357b8d309d9ba03ca553544048ff
Sivanchan5 <sivanchan5@gmail.com>
2024-09-02 17:05:31 -0700

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -36,8 +36,8 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://www.loveandoliveoil.com/wp-content/uploads/2017/09/kitchen-essentialsH.jpg" // Replace with your category image
                 alt="Kitchen Essentials"
                 style={{
-                  width: "95%",
-                  height: "100%",
+                  width: "70%",
+                  height: "80%",
                   objectFit: "cover",
                   borderRadius: "10px",
                   marginBottom: "8px",
@@ -59,8 +59,8 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://www.sleepcheapmattresses.com/wp-content/uploads/2023/02/Coaster-Bedroom-Furniture-1.jpeg" // Replace with your category image
                 alt="Furniture"
                 style={{
-                  width: "80%",
-                  height: "70%",
+                  width: "58%",
+                  height: "80%",
                   objectFit: "cover",
                   borderRadius: "10px",
                   marginBottom: "8px",
@@ -82,7 +82,7 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ09dDDRtJeRvkgUVPKYvyA5dexA_YpPc2zyw&s" // Replace with your category image
                 alt="Electronics"
                 style={{
-                  width: "80%",
+                  width: "58%",
                   height: "100%",
                   objectFit: "cover",
                   borderRadius: "10px",
@@ -105,7 +105,7 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://images.herzindagi.info/image/2018/Nov/lights-decor-wedding.jpg" // Replace with your category image
                 alt="Lighting & Decor"
                 style={{
-                  width: "80%",
+                  width: "70%",
                   height: "100%",
                   objectFit: "cover",
                   borderRadius: "10px",
@@ -128,7 +128,7 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://img.fruugo.com/product/1/30/716586301_max.jpg" // Replace with your category image
                 alt="Bedding"
                 style={{
-                  width: "65%",
+                  width: "57%",
                   height: "60%",
                   objectFit: "cover",
                   borderRadius: "10px",
@@ -151,7 +151,7 @@ function Homepage({ isLoggedIn, onLogout }) {
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd9iYQX7Nc3SGxqvBvsuz9FvOIDH4TdFDetA&s" // Replace with your category image
                 alt="Home Organizing"
                 style={{
-                  width: "115%",
+                  width: "100%",
                   height: "100%",
                   objectFit: "cover",
                   borderRadius: "10px",
